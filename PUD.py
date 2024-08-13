8314973491ba2149b84a3dd2a5c4638052839dc5
lungcollapser <milesterrell1337@gmail.com>
2024-08-13 07:59:05 -0500

    pud

--- PUD.py
+++ PUD.py
@@ -6,8 +6,7 @@ war_name = "Warrior"
 
 player = ""
 class_choice = ""
-yes = "Y", "n"
-no = "N", "n"
+yes_no = "Y", "y", "N", "n"
 
 wizard_stats = [10, 14, 12, 20, 16, 18]
 thief_stats = [10, 20, 14, 12, 16, 18]
@@ -21,9 +20,11 @@ class_choice = input("What is your class?")
 
 while class_choice:
     if class_choice == wiz_name:
-        print("A wizard? Are you sure?")
-        if yes = input("Y") or yes = input("y"):
-            print("Very good.")
+        yes_no = input("A wizard? Are you sure? Y or N?")
+        if yes_no == "N" or "n":
+            class_choice = input("Please type in one of the class names.")
+        else:
+            print("Very well.")
         break
     elif class_choice == war_name:
         print("A warrior? Are you sure?")
