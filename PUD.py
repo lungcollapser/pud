03e8b3d6321f791370719824d9d18013f79d6915
lungcollapser <milesterrell1337@gmail.com>
2024-08-13 07:51:58 -0500

    break ty stack lol

--- PUD.py
+++ PUD.py
@@ -1,33 +1,42 @@
-
-
 class_list = ["Wizard", "Warrior", "Thief"]
 order_of_stats = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"]
-
+wiz_name = "Wizard"
+thief_name = "Thief"
+war_name = "Warrior"
 
 player = ""
-
+class_choice = ""
+yes = "Y", "n"
+no = "N", "n"
 
 wizard_stats = [10, 14, 12, 20, 16, 18]
 thief_stats = [10, 20, 14, 12, 16, 18]
 war_stats = [20, 16, 18, 10, 12, 14]
 
-print(">Welcome to PUD")
-print(">What is your name?")
-input(player)
-print(">...Very interesting. What is your class?")
+print("Welcome to PUD")
+player = input("What is your name?")
+print("...Very interesting.")
 print(class_list)
-class_choice = input("> ")
-while class_choice:
-    if class_choice == "Wizard":
-        print("A wizard? Are you sure?")
-    elif class_choice == "Warrior":
-        print("A warrior? Are you sure?")
-    elif class_choice == "Thief":
-        print("A thief? Are you sure?")
-    else:
-        print("Please type in one of the class names.")
-print(class_list)
+class_choice = input("What is your class?")
 
+while class_choice:
+    if class_choice == wiz_name:
+        print("A wizard? Are you sure?")
+        if yes = input("Y") or yes = input("y"):
+            print("Very good.")
+        break
+    elif class_choice == war_name:
+        print("A warrior? Are you sure?")
+        break
+    elif class_choice == thief_name:
+        print("A thief? Are you sure?")
+        break
+    else:
+        class_choice = input("Please type in one of the class names.")
+
+    print(class_list)
+
+print("Excellent! Enjoy your adventure.")
 
 
 
