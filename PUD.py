29ca34a329838dbb5881782b95124b49c4e6bae7
Miles Terrell <milesterrell1337@gmail.com>
2024-08-16 10:59:22 -0500

    adventure awaits

--- PUD.py
+++ PUD.py
@@ -1,7 +1,9 @@
 import chooseClass
 
 class_list = ["Wizard", "Warrior", "Thief"]
-order_of_stats = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"]
+wiz_stats = {"Strength": 10, "Dexterity": 14, "Constitution": 12, "Intelligence": 20, "Wisdom": 16, "Charisma": 18}
+thief_stats = {"Strength": 10, "Dexterity": 20, "Constitution": 14, "Intelligence": 12, "Wisdom": 16, "Charisma": 18}
+warrior_stats = {"Strength": 20, "Dexterity": 16, "Constitution": 18, "Intelligence": 10, "Wisdom": 12, "Charisma": 14}
 wiz_name = "Wizard"
 thief_name = "Thief"
 war_name = "Warrior"
@@ -17,25 +19,42 @@ class_choice = input("What is your class?")
 
 while class_choice:
     if class_choice == wiz_name:
+        print(wiz_stats)
         yes_no = input("A wizard? Are you sure? Type Y or N.")
         if yes_no == "Y":
-            print("Excellent")
+            print("Spell-casters do not last very long here")
         elif yes_no == "N":
             print(class_list)
             print("Type in your class.")
         break
     elif class_choice == war_name:
-        print("A warrior? Are you sure?")
+        print(warrior_stats)
+        yes_no = input("A warrior? Are you sure? Type Y or N.")
+        if yes_no == "Y":
+            print("Brute strength suits your demeanor")
+        elif yes_no == "N":
+            print(class_list)
+            print("Type in your class.")
         break
     elif class_choice == thief_name:
-        print("A thief? Are you sure?")
+        print(thief_stats)
+        yes_no = input("A thief? Are you sure? Type Y or N.")
+        if yes_no == "Y":
+            print("Hmmm.. Not all that surprising.")
+        elif yes_no == "N":
+            print(class_list)
+            print("Type in your class.")
         break
     else:
         class_choice = input("Please type in one of the class names.")
+        continue
 
-    print(class_list)
+yes_no: str = input("All set? Type Y to start your adventure.")
 
-print("Excellent! Enjoy your adventure.")
+yes_no: str = input("You awake in a daze. The ceiling fan loudly swirling around your room. 'I am going to be late again."
+                    "letting Roman pressure me into excessive drinking?' You get out of your bed and look around your room."
+                    "It's not too terribly messy, but you could clean up. Your eyes fixate on the floor. What would you like"
+                    "to do?")
 
 
 
