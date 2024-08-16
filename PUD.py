0f719a0dca2c7ec681df359c8ebe68ebee5803e4
Miles Terrell <milesterrell1337@gmail.com>
2024-08-16 13:54:19 -0500

    class implementation

--- PUD.py
+++ PUD.py
@@ -1,4 +1,14 @@
-import chooseClass
+import random
+
+class Wizard:
+    def __init__(self, stats, health, armor_class, weapon, weapon_damage):
+        self.stats = stats
+        self.health = health
+        self.armor_class = armor_class
+        self.weapon = weapon
+        self.weapon_damage = weapon_damage
+
+wizard_sheet = Wizard({"Strength": 10, "Dexterity": 14, "Constitution": 12, "Intelligence": 20, "Wisdom": 16, "Charisma": 18}, 12, 7, "Quarterstaff" )
 
 class_list = ["Wizard", "Warrior", "Thief"]
 wiz_stats = {"Strength": 10, "Dexterity": 14, "Constitution": 12, "Intelligence": 20, "Wisdom": 16, "Charisma": 18}
