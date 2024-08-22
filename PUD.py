07f7d63a1969f27e7fbcd8211c24882d2a0f4903
lungcollapser <milesterrell1337@gmail.com>
2024-08-21 22:25:49 -0500

    ayo

--- PUD.py
+++ PUD.py
@@ -7,14 +7,18 @@ wiz = WizardCharacterSheet.Wizard
 thief = ThiefCharacterSheet.Thief
 war = WarriorCharacterSheet.Warrior
 
-wiz_pattern = re.compile(r".*Wizard$")
-thief_pattern = re.compile(r".*Thief$")
-war_pattern = re.compile(r".*Warrior$")
+wiz_pattern = re.compile(r".*wizard$")
+thief_pattern = re.compile(r".*thief$")
+war_pattern = re.compile(r".*warrior$")
 yes_pattern = re.compile(r"^.*yes")
 no_pattern = re.compile(r"^.*no")
 
-class_list = ["Wizard", "Warrior", "Thief"]
+royalty_pattern = re.compile(r"^.*royalty")
+peasant_pattern = re.compile(r"^.*peasant")
+commoner_pattern = re.compile(r"^.*commoner")
 
+class_list = ["wizard", "warrior", "thief"]
+path_list = ["royalty", "peasant", "commoner"]
 
 print("welcome to PUD")
 player = input("what is your name?")
@@ -26,7 +30,7 @@ def choose_class():
     class_choice = input("what is your class?")
     if re.search(wiz_pattern, class_choice):
         print(wiz.wizard_sheet)
-        yes_no = input("are you sure? yes or no?")
+        yes_no = input("are you sure? type yes or no.")
         if re.search(yes_pattern, yes_no):
             print("spell-casters don't last long here.")
         elif re.search(no_pattern, yes_no):
@@ -35,16 +39,16 @@ def choose_class():
             choose_class()
     elif re.search(war_pattern, class_choice):
         print(war.warrior_sheet)
-        yes_no = input("are you sure? yes or no?")
+        yes_no = input("are you sure? type yes or no.")
         if re.search(yes_pattern, yes_no):
-            print("brute strength suits you.")
+            print("another hero with a sword and shield in line for his death.")
         elif re.search(no_pattern, yes_no):
             choose_class()
         else:
             choose_class()
     elif re.search(thief_pattern, class_choice):
         print(thief.thief_sheet)
-        yes_no = input("are you sure? yes or no?")
+        yes_no = input("are you sure? type yes or no.")
         if re.search(yes_pattern, yes_no):
             print("don't go looking into my pockets.")
         elif re.search(no_pattern, yes_no):
@@ -57,19 +61,54 @@ choose_class()
 
 
 def beginning():
-    print("yoyoyo bitch")
+    print("yoyoyo")
 
+
+def which_path():
+    print("now decide your path to start on. this will greatly alter your experience, so choose wisely.")
+    print(path_list)
+    choose_path = input("")
+    if re.search(royalty_pattern, choose_path):
+        yes_no = input("royalty? are you sure? type yes or no.")
+        if re.search(yes_pattern, yes_no):
+            print("wealth is a shortcut to an early grave. watch your back.")
+        elif re.search(no_pattern, yes_no):
+            which_path()
+        else:
+            which_path()
+    if re.search(peasant_pattern, choose_path):
+        yes_no = input("peasant? are you sure? type yes or no.")
+        if re.search(yes_pattern, yes_no):
+            print("you'll be rolling in shit before you know it.")
+        elif re.search(no_pattern, yes_no):
+            which_path()
+        else:
+            which_path()
+    if re.search(commoner_pattern, choose_path):
+        yes_no = input("commoner? are you sure? type yes or no.")
+        if re.search(yes_pattern, yes_no):
+            print("ahhh, a perfectly normal and average life. how underwhelming.")
+        elif re.search(no_pattern, yes_no):
+            which_path()
+        else:
+            which_path()
 
 def are_you_ready():
-    yes_no = input("are you ready to begin? yes or no?.")
+    yes_no = input("are you ready to begin? type yes or no.")
     if re.search(yes_pattern, yes_no):
         beginning()
     elif re.search(no_pattern, yes_no):
-        yes_no = input("Would you like to change your class?")
+        yes_no = input("would you like to change your class?")
         if re.search(yes_pattern, yes_no):
             choose_class()
-        if re.search(no_pattern, yes_no):
             are_you_ready()
+        if re.search(no_pattern, yes_no):
+            yes_no = input("would you like to change your path?")
+            if re.search(yes_pattern, yes_no):
+                which_path()
+            elif re.search(no_pattern, yes_no):
+                are_you_ready()
 
 
+which_path()
 are_you_ready()
