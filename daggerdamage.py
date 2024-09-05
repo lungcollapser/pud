23bac62015f710c2bb1513a8d1d86b7ba24c8717
lungcollapser <milesterrell1337@gmail.com>
2024-09-04 19:31:32 -0500

    damage calculations

--- daggerdamage.py
+++
@@ -1,6 +0,0 @@
-import random
-
-
-def dagger_damage():
-    stab = random.randint(0, 6)
-    print(stab)
---
+++ damagecalculations.py
@@ -0,0 +1,31 @@
+import random
+
+
+def coin_flip():
+    print(random.randint(0, 1))
+
+
+def one_d_four():
+    print(random.randint(0, 4))
+
+
+def one_d_six():
+    print(random.randint(0, 6))
+
+
+def one_d_eight():
+    print(random.randint(0, 8))
+
+
+def one_d_ten():
+    print(random.randint(0, 10))
+
+
+def one_d_twelve():
+    print(random.randint(0, 12))
+
+
+def one_d_twenty():
+    print(random.randint(0, 20))
+
+
