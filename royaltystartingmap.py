73491dbb1ea04f4f7ac855c519b86d9b8fa473bc
lungcollapser <milesterrell1337@gmail.com>
2024-09-05 21:58:04 -0500

    royalty room class

--- royaltystartingmap.py
+++ royaltystartingmap.py
@@ -1,7 +1,11 @@
-class RoyaltyMap:
+class RoyaltyRoom:
 
-    def __init__(self, door, cabinet, shoes, glass, plate, wall, ):
+    def __init__(self, door, storage_container, bed, wall):
         self.door = door
-        self.cabinet = cabinet
+        self.storage_container = storage_container
+        self.bed = bed
+        self.wall = wall
 
+
+royalty_room = RoyaltyRoom({"door", "storage container", "bed", "wall"})
 
